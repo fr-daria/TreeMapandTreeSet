04a603ffbd76ec13200001d0249a42709e50bc05
fr-daria <frantsuzova00000@yandex.ru>
2023-03-23 00:10:36 +0300

    Доработки

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,20 @@ public class Main {
         list.add(new Person("Борис", "Дояркин", 56));
         list.add(new Person("Ларса", "Шевцова", 48));
 
-        Collections.sort(list, Comparator.comparingInt(o -> o.getSurname().length()));
+        list.sort((o1, o2) -> {
+            String[] worldOfSurname1 = o1.getSurname().split(" ");
+            if (worldOfSurname1.length >= 3) {
+                System.out.println("Много слов в фамилии");
+            }
+            String[] worldOfSurname2 = o2.getSurname().split(" ");
+            if (worldOfSurname2.length >= 3) {
+                System.out.println("Много слов в фамилии");
+            }
+            if (o1.getSurname().length() == o2.getSurname().length()) {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            }
+            return Integer.compare(o1.getSurname().length(), o2.getSurname().length());
+        });
 
         Predicate<Person> predicate = (Person person) -> person.getAge() < 18;
 
